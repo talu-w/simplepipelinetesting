45d90f164634c1b86a75bf019a4b8f9b08829d2f
Talu <talu.whitley@gmail.com>
2026-02-04 14:56:12 -0700

    Update simple1.py
    Added configs

--- simple1.py
+++ simple1.py
@@ -1,1 +1,25 @@
+from jinja2.environment import Template
+from nornir import InitNornir
+from nornir_scrapli.tasks import send_configs
+from nornir_utils.plugins.functions import print_result
+from nornir_utils.plugins.tasks.data import load_yaml
+from nornir_jinja2.plugins.tasks import template_file
+from nornir.core.exceptions import NornirExecutionError
 
+nr = InitNornir(config_file="config.yaml")
+
+def pull_vars(task):
+    result = task.run(task=load_yaml, file="group_vars/all.yaml")
+    task.host["facts"] = result.result
+    push_config(task)
+
+def push_config(task):
+    loopback_configs = task.run(task=template_file, template="loopback.j2", path="templates")
+    configurations = loopback_configs.result.splitlines()
+    task.run(task=send_configs, configs=configurations)
+
+results = nr.run(task=pull_vars)
+print_result(results)
+failures = nr.data.failed_hosts
+if failures:
+    raise NornirExecutionError("Nornir Failure detected")
